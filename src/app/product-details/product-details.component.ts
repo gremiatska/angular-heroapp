cec9af6220a1bfa97b9dbdc6602d78e47cfcebd1
gremiatska <yulia.chechun@gmail.com>
2021-08-17 14:49:49 +0300

    3 files modified, 2 added

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -14,7 +14,7 @@ export class ProductDetailsComponent implements OnInit {
   constructor(
     private route: ActivatedRoute,
     private cartService: CartService
-  ) {}
+  ) { }
 
   addToCart(product: Product) {
     this.cartService.addToCart(product);
